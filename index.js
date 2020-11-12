e42b6a10366354b4f737480cb4c579682be6cd7d
Kris M <kzzm@users.noreply.github.com>
2020-11-12 10:12:34 -0600

    Updated project to add defining and sending name to chat stream.
    Included extra button in index html to define name. Extra id sent so that name is  included as part of the chat stream.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//const activeUsers = new Set();
+
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
